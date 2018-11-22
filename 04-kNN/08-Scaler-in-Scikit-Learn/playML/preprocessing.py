2756854e11fe9dfb58ccf8ba972d43992074422a
wangsheng <wangsheng@zichan360.com>
2018-11-22 10:38:44 +0800

    增加了代码

---
+++ 04-kNN/08-Scaler-in-Scikit-Learn/playML/preprocessing.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+class StandardScaler:
+
+    def __init__(self):
+        self.mean_ = None
+        self.scale_= None
+
+    def fit(self,X):
+        """根据训练数据集X获得数据的均值和方差"""
+        assert X.ndim == 2, "The dimension of X must be 2"
+
+        self.mean_ = np.array([np.mean(X[:,i]) for i in range(X.shape[1])])
+        self.scale_ = np.array([np.std(X[:, i]) for i in range(X.shape[1])])
+
+        return self
+
+    def transform(self,X):
+        """将X根据这个StandardScaler进行均值方差归一化处理"""
+        assert X.ndim == 2, "The dimension of X must be 2"
+        assert self.mean_ is not None and self.scale_ is not None, \
+            "must fit before transform!"
+        assert X.shape[1] == len(self.mean_), \
+            "the feature number of X must be equal to mean_ and std_"
+
+        resX=np.empty(shape=X.shape,dtype=float)
+        for col in range(X.shape[1]):
+            resX[:,col]=(X[:,col]-self.mean_[col])/self.scale_[col]
+        return resX
+
+
+class MinMaxScaler:
+
+    def __init__(self):
+        self._min=None
+        self._max=None
+
+    def fit(self,X):
+        """根据训练数据集X获得数据的均值和方差"""
+        assert X.ndim == 2, "The dimension of X must be 2"
+
+        self._min= np.array([np.min(X[:,i]) for i in range(X.shape[1])])
+        self._max = np.array([np.max(X[:, i]) for i in range(X.shape[1])])
+
+        return self
+
+    def transform(self,X):
+        """将X根据这个StandardScaler进行均值方差归一化处理"""
+        assert X.ndim == 2, "The dimension of X must be 2"
+        assert self._min is not None and self._max is not None, \
+            "must fit before transform!"
+        assert X.shape[1] == len(self._min), \
+            "the feature number of X must be equal to mean_ and std_"
+
+        resX=np.empty(shape=X.shape,dtype=float)
+        for col in range(X.shape[1]):
+            resX[:,col]=(X[:,col]-self._min[col])/(self._max[col]-self._min[col])
+        return resX
